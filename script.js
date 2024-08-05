28d26a89eca92f28eeeae86fb3ad03cf86008899
V-M-B <vmbvarun@gmail.com>
2024-08-05 23:02:53 +0530

    VS-Code-Landing-Page-ReDesign

---
+++ script.js
@@ -0,0 +1,19 @@
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+// let totalsum=sum('ten',15)
+// console.log(totalsum)
+
+function age(m)
+{
+    if(m>=18)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+let myage=(0)
+console.log(myage)
